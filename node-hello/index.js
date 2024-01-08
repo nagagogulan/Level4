3bbe77ab4423454376cfa28ce2270ee7062382de
nagagogulan <142006979+nagagogulan@users.noreply.github.com>
2024-01-08 18:06:27 +0530

    Update index.js

--- node-hello/index.js
+++ node-hello/index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node to evetryone!\n'
+  const msg = 'Hello Node to everyone!\n';
   res.end(msg);
 });
 
