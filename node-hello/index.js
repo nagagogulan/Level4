e46ae788b3a0651d20d2e05b61677881f72a8ba4
nagagogulan <142006979+nagagogulan@users.noreply.github.com>
2024-01-08 17:29:17 +0530

    Update index.js

--- node-hello/index.js
+++ node-hello/index.js
@@ -1,10 +1,6 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {console.log(`Server is running on port ${port}`);
-});
-
-
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   const msg = 'Hello Node to evetryone!\n'
